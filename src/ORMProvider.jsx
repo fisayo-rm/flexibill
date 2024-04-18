3fa886e045db969a46d2092eec3307f5d776a98d
Fisayo <aikomofisayo@gmail.com>
2024-04-18 11:34:04 +0100

    Add useEffect hook to fetch and update invoices in ORMProvider

--- src/ORMProvider.jsx
+++ src/ORMProvider.jsx
@@ -1,14 +1,31 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import orm from "./models";
 
 import PropTypes from "prop-types";
 import { ORMContext } from "./ORMContext";
+import InvoiceService from "./services/InvoiceService";
 
 export const ORMProvider = ({ children }) => {
-  const [state, setState] = useState(orm.getEmptyState());
+  // using null instead orm.getEmptyState() so we can initialize state with a falsy value
+  const [state, setState] = useState(null);
+  const [errors, setErrors] = useState({});
+
+  useEffect(() => {
+    const fetchState = async () => {
+      const session = orm.session(state);
+      const invoices = await InvoiceService.getInvoices();
+      invoices.forEach((invoice) => {
+        session.Invoice.upsert(invoice);
+      });
+      setState(session.state);
+    };
+    if (!state) {
+      fetchState();
+    }
+  }, [state]);
 
   return (
-    <ORMContext.Provider value={{ state, setState }}>
+    <ORMContext.Provider value={{ state, setState, errors, setErrors }}>
       {children}
     </ORMContext.Provider>
   );
