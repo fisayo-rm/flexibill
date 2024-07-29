a6e3786b19e01f6134724109b17794f6984c9ae5
Fisayo <aikomofisayo@gmail.com>
2024-07-29 14:57:09 +0100

    Verify react-context-orm functionality

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,20 @@ import "./App.scss";
 import { Route, Routes } from "react-router-dom";
 import Invoices from "./Invoices";
 import Invoice from "./Invoice";
+import { StoreProvider } from "react-context-orm";
 
 function App() {
   return (
-    <div className="min-vh-50">
-      <Routes>
-        <Route path="/" element={<Dashboard />}>
-          <Route path="/invoices" element={<Invoices />} />
-          <Route path="/invoice" element={<Invoice />} />
-        </Route>
-      </Routes>
-    </div>
+    <StoreProvider>
+      <div className="min-vh-50">
+        <Routes>
+          <Route path="/" element={<Dashboard />}>
+            <Route path="/invoices" element={<Invoices />} />
+            <Route path="/invoice" element={<Invoice />} />
+          </Route>
+        </Routes>
+      </div>
+    </StoreProvider>
   );
 }
 
